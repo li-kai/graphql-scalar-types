26922d86bf2a2d4bd49918029790c5fdeb9d6109
Li Kai <falconets25601@gmail.com>
2018-03-23 17:51:40 +0800

    Simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
-  coverageDirectory: 'coverage',
-  moduleNameMapper: {
-    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|bmp|ico|yml)$': '<rootDir>/__mocks__/fileMock.js',
-    '^.+\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-  },
-  modulePaths: [
-    '<rootDir>/src',
-  ],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.js',
-  ],
-  testRegex: '(/__tests__/.*\\.test.js)$',
+  modulePaths: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.js"],
+  testRegex: "(/__tests__/.*\\.test.js)$", // Only write lcov files in CIs
+  coverageReporters: ["text"].concat(process.env.CI ? "lcov" : [])
 };
